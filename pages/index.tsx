2cbc106eda15c7ce4797e2f843655e00750a48b5
Milan Poudel <milanpoudel.safalza@gmail.com>
2022-06-30 11:32:52 +0545

    Create dev branch

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import { NextPage } from "next";
 import React from "react";
 
 const Home: NextPage = () => {
-  return <div>Hey welcome to the app</div>;
+  return <div>Hey welcome to the app development</div>;
 };
 
 export default Home;
