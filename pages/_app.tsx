2c2f2c860d65d96b0c1ed26ae9922e8026962883
milan poudel <milanwebdeveloper1@gmail.com>
2022-07-08 15:12:15 +0545

    feature/use-of-react-query-in-fetching-messages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,22 +8,27 @@ import '@fontsource/work-sans/500.css'
 import '@fontsource/work-sans/700.css'
 
 import ChatProvider from '../Context/ChatProvider'
+import { QueryClientProvider, QueryClient } from 'react-query'
+
+const queryClient = new QueryClient()
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
-      <ChatProvider>
-        <Box
-          minH="100vh"
-          display="flex"
-          backgroundImage="/images/background.png"
-          backgroundSize="cover"
-          backgroundPosition="center"
-        >
-          <Component {...pageProps} />
-        </Box>
-      </ChatProvider>
-    </ChakraProvider>
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider theme={theme}>
+        <ChatProvider>
+          <Box
+            minH="100vh"
+            display="flex"
+            backgroundImage="/images/background.png"
+            backgroundSize="cover"
+            backgroundPosition="center"
+          >
+            <Component {...pageProps} />
+          </Box>
+        </ChatProvider>
+      </ChakraProvider>
+    </QueryClientProvider>
   )
 }
 
--- utils/chat.ts
+++ utils/chat.ts
@@ -3,7 +3,7 @@ import { IUser } from '../Context/ChatProvider'
 export const isSameSender = (messages: any[], index: number, user: IUser) => {
   return (
     index < messages.length - 1 &&
-    messages[index + 1].sender._id === messages[index].sender._id &&
+    messages[index + 1]?.sender._id === messages[index].sender._id &&
     messages[index].sender._id !== user._id
   )
 }
